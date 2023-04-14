ff4eef3b952ccd2f7f9725790bd2a0e16d3fad14
Kai <KKoehler@lsst.org>
2023-04-14 21:02:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Semaphoreupdatebot
+
+Slack bot designed to post broadcast messages to a configured Slack channel when a broadcast message becomes active for a specific environment.
+Learn more at https://semaphoreupdatebot.lsst.io
+
+Semaphoreupdatebot is developed with [FastAPI](https://fastapi.tiangolo.com) and [Safir](https://safir.lsst.io).
---
+++ src/semaphoreupdatebot/main.py
@@ -0,0 +1,51 @@
+"""The main application factory for the Semaphoreupdatebot service.
+
+Notes
+-----
+Be aware that, following the normal pattern for FastAPI services, the app is
+constructed when this module is loaded and is not deferred until a function is
+called.
+"""
+
+from importlib.metadata import metadata, version
+
+from fastapi import FastAPI
+from safir.dependencies.http_client import http_client_dependency
+from safir.logging import configure_logging, configure_uvicorn_logging
+from safir.middleware.x_forwarded import XForwardedMiddleware
+
+from .config import config
+from .handlers.external import external_router
+from .handlers.internal import internal_router
+
+__all__ = ["app", "config"]
+
+
+configure_logging(
+    profile=config.profile,
+    log_level=config.log_level,
+    name="semaphoreupdatebot",
+)
+configure_uvicorn_logging(config.log_level)
+
+app = FastAPI(
+    title="Semaphoreupdatebot",
+    description=metadata("Semaphoreupdatebot")["Summary"],
+    version=version("Semaphoreupdatebot"),
+    openapi_url=f"/{config.path_prefix}/openapi.json",
+    docs_url=f"/{config.path_prefix}/docs",
+    redoc_url=f"/{config.path_prefix}/redoc",
+)
+"""The main FastAPI application for Semaphoreupdatebot."""
+
+# Attach the routers.
+app.include_router(internal_router)
+app.include_router(external_router, prefix=f"/{config.path_prefix}")
+
+# Add middleware.
+app.add_middleware(XForwardedMiddleware)
+
+
+@app.on_event("shutdown")
+async def shutdown_event() -> None:
+    await http_client_dependency.aclose()
